c39ae8445e63e55fdbf6db37dbfea5672902dc6e
Jalal Kiswani <kiswanij@gmail.com>
2021-03-13 20:23:46 +0200

    .

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -10,44 +10,42 @@ import com.jk.web.services.client.JKServiceClient;
 
 public class App {
 	private static int id;
+
 	public static void main(String[] args) {
-		ServiceClient client=new ServiceClient();
+		ServiceClient client = new ServiceClient();
 		addRecord(client);
 		printAll(client);
-		find(client);		
+		find(client);
 		delete(client);
-		//find(client); this will throw an exception with 404 page not found
-		
+		// find(client); this will throw an exception with 404 page not found
 	}
 
 	private static void delete(ServiceClient client) {
-		client.callJsonWithDelete("/"+id);		
+		client.delete(id);
 	}
 
 	private static void find(ServiceClient client) {
 		// Retrieve single account
-		Model account = client.callSingleJson("/"+id);
+		Model account = client.find("/" + id);
 		JK.printBlock(account);
 	}
 
 	private static void addRecord(ServiceClient client) {
-		Model account=new Model();
+		Model account = new Model();
 		account.setNumber("1234");
 		account.setName("Jalal");
 		account.setAvg(100.3);
 		account.setPhone("88888");
-		String response= client.callJsonWithPost(account);
-		
-		
-		account = JKObjectUtil.jsonToObject(response, Model.class);
-		id=account.getId();//keep the current id for the later calls
+		account = client.insert(account);
+
+		id = account.getId();// keep the current id for the later calls
 	}
 
 	private static void printAll(ServiceClient client) {
 		// Retrieve all account
-		List<Model> list = client.callJsonAsListOfObjects();
+		List<Model> list = client.getAll();
 
-		for (Model record: list) {
+		for (Model record : list) {
 			JK.print(record);
 		}
 	}
--- src/main/java/com/app/account/ServiceClient.java
+++ src/main/java/com/app/account/ServiceClient.java
@@ -1,6 +1,5 @@
 package com.app.account;
 
-import com.jk.util.JKObjectUtil;
 import com.jk.web.services.client.JKMatureServiceClient;
 
 public class ServiceClient extends JKMatureServiceClient<Model>{
